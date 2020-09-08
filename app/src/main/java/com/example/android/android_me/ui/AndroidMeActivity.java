f7fd41c6ae037141ba9ef22d1553cd69a4ec9e8a
SonNXP <son.nguyen.xp12@gmail.com>
2020-09-08 15:23:14 +0700

    done exercise 1

--- app/src/main/java/com/example/android/android_me/ui/AndroidMeActivity.java
+++ app/src/main/java/com/example/android/android_me/ui/AndroidMeActivity.java
@@ -16,6 +16,7 @@
 
 package com.example.android.android_me.ui;
 
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -32,13 +33,16 @@ public class AndroidMeActivity extends AppCompatActivity {
         // TODO (3) Show the first image in the list of head images
             // Soon, you'll update this image display code to show any image you want
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_android_me);
 
         // TODO (5) Create a new BodyPartFragment instance and display it using the FragmentManager
+        BodyPartFragment headFragment = new BodyPartFragment();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .add(R.id.head_container, headFragment)
+                .commit();
     }
 }
